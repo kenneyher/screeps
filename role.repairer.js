f5836990017185aaedae86413c741dbfef5fc168
Kenneth Herrera <69918528+kenneyher@users.noreply.github.com>
2023-03-27 22:35:31 -0600

    add roles

---
+++ role.repairer.js
@@ -0,0 +1,35 @@
+var builder = require('role.builder');
+var roleRepairer = {
+    run: function(creep){
+        if(creep.memory.repairing && creep.store[RESOURCE_ENERGY] == 0){
+            creep.memory.repairing = false;
+            creep.say('🔄');
+        }else if(!creep.memory.repairing && creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0){
+            creep.memory.repairing = true;
+            creep.say('🛠');
+        }
+        
+        if(creep.memory.repairing){
+            var targets = creep.room.find(FIND_MY_STRUCTURES, {
+                filter: (s) => (s.structureType != STRUCTURE_WALL) && s.hits < s.hitsMax
+            });
+            target = targets[0];
+            if(target){
+                var e = creep.repair(target);
+                if(e == ERR_NOT_IN_RANGE){
+                    creep.moveTo(target);
+                }
+            }else {
+                builder.run(creep);
+            }
+        }else{
+            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+            if(source != undefined){
+                if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(source);
+                }
+            }
+        }
+    }
+};
+module.exports = roleRepairer;
